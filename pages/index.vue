6a9275bca47cb43899986bad661e3226d745ab77
MahmudJion <mahmudh.jion02@gmail.com>
2023-06-01 20:15:55 +0100

    animation added

--- pages/index.vue
+++ pages/index.vue
@@ -2,45 +2,46 @@
     <div>
       <b-container>
         <div>
-          <b-jumbotron header="Hello 👋" lead="">
+          <b-jumbotron header="Hello 👋" lead="" class="fade-in">
             <b-navbar type="light" variant="warning">
               <h2>I'm Mahmud Hasan Jion,</h2>
             </b-navbar>
             <br />
             <p>
               A Software Engineer with over three years of experience in
-              software and web development. <br /> I am currently pursuing my Master's in Software Engineering at the <a href="https://www.herts.ac.uk/" style="text-decoration: none; color:#703C84" target="_blank">University of Hertfordshire</a>.
-              I completed my Bachelor's in Computer Science & Engineering from <a href="https://www.aiub.edu/" style="text-decoration: none; color:#224077" target="_blank">American International University-Bangladesh (AIUB)</a> and have since been working as a Web Developer. I have experience building multi-vendor E-commerce platforms.
+              software and web development. <br /> I am currently doing my Master's in Software Engineering at the <a href="https://www.herts.ac.uk/" style="text-decoration: none; color:#703C84" target="_blank">University of Hertforshire</a>.
+              I have completed my Bachelor in Computer Science & Engineering from <a href="https://www.aiub.edu/" style="text-decoration: none; color:#224077" target="_blank">American International University-Bangladesh (AIUB)</a> and since then working as Web Developer
+              and had some experience on building multi-vendor E-commerce platforms.
             </p>
             <h4>Currently working with</h4>
             <b-navbar type="light" variant="warning">
               <h3>
-                PHP, CodeIgniter, Laravel, JavaScript, Vue.js, React, Next.js, Nuxt.js, Node.js, Ruby, and Python
+                PHP, Codeigniter, Laravel, Javascript, Vue.js, React, Next/Nuxt, Node.js, Ruby and Python
               </h3>
             </b-navbar>
             <br />
             <div>
-              <h4>Check out my work and profile</h4>
-              <ul class="ul">
-                <li>
-                  <a href="https://github.com/MahmudJion" style="text-decoration: none; color:#24292E" target="_blank">GitHub</a>
-                </li>
-                <li>
-                  <a href="https://www.linkedin.com/in/mahmud-hasan-jion/" style="text-decoration: none; color:#0A66C2" target="_blank">LinkedIn</a>
-                </li>
-                <li>
-                  <a href="https://twitter.com/mahmudhasanjion" style="text-decoration: none; color:#1A91DA" target="_blank">Twitter</a>
-                </li>
-                <li>
-                  <a href="https://stackoverflow.com/users/10030431/mahmud-hasan-jion" style="text-decoration: none; color:#F3862F" target="_blank">Stack Overflow</a>
-                </li>
-                <li>
-                  <a href="https://scholar.google.com/citations?user=rvFKahIAAAAJ&hl=en" style="text-decoration: none; color:#767676" target="_blank">Google Scholar</a>
-                </li>
-                <li>
-                  <a href="https://herts.academia.edu/MahmudHasanJion" style="text-decoration: none; color:#3C3F42" target="_blank">Academia.edu</a>
-                </li>
-              </ul>
+              <h4>Check my works and profile</h4>
+                <ul class="ul">
+                  <li>
+                    <a href="https://github.com/MahmudJion" style="text-decoration: none; color:#24292E" target="_blank">GitHub</a>
+                  </li>
+                  <li>
+                    <a href="https://www.linkedin.com/in/mahmud-hasan-jion/" style="text-decoration: none; color:#0A66C2" target="_blank">Linkedin</a>
+                  </li>
+                  <li>
+                    <a href="https://twitter.com/mahmudhasanjion" style="text-decoration: none; color:#1A91DA" target="_blank">Twitter</a>
+                  </li>
+                  <li>
+                    <a href="https://stackoverflow.com/users/10030431/mahmud-hasan-jion" style="text-decoration: none; color:#F3862F" target="_blank">Stack Overflow</a>
+                  </li>
+                  <li>
+                    <a href="https://scholar.google.com/citations?user=rvFKahIAAAAJ&hl=en" style="text-decoration: none; color:#767676" target="_blank">Google Scholar</a>
+                  </li>
+                  <li>
+                    <a href="https://herts.academia.edu/MahmudHasanJion" style="text-decoration: none; color:#3C3F42" target="_blank">Academia.edu</a>
+                  </li>
+                </ul>
             </div>
           </b-jumbotron>
         </div>
@@ -49,10 +50,15 @@
   </template>
 
   <script>
-  export default {};
+  export default {
+    mounted() {
+      const jumbotron = document.querySelector('.fade-in');
+      jumbotron.classList.add('animate__animated', 'animate__fadeIn');
+    }
+  }
   </script>
 
-  <style scoped>
+  <style>
   .container {
     margin: 8px;
     padding: 5vh 10vw;
@@ -61,7 +67,3 @@
     justify-content: right;
     align-items: center;
   }
-  .ul {
-    list-style-type: square;
-  }
-  </style>
