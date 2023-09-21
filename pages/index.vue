a292e6f0a27c81b642820a6337240675b4742217
Mahmud Hasan Jion <mahmudh.jion02@gmail.com>
2023-09-21 23:19:04 +0100

    update index.vue with previous template

--- pages/index.vue
+++ pages/index.vue
@@ -2,24 +2,44 @@
     <div>
       <b-container>
         <div>
-          <b-jumbotron header="" lead="" class="fade-in">
+          <b-jumbotron header="Hello 👋" lead="">
             <b-navbar type="light" variant="warning">
-              <h2></h2>
+              <h2>I'm Mahmud Hasan Jion,</h2>
             </b-navbar>
             <br />
             <p>
-              
+              A Software Engineer with over three years of experience in
+              software and web development. <br /> I am currently doing my Master's in Software Engineering at the <a href="https://www.herts.ac.uk/" style="text-decoration: none; color:#703C84" target="_blank">University of Hertforshire</a>.
+              I have completed my Bachelor in Computer Science & Engineering from <a href="https://www.aiub.edu/" style="text-decoration: none; color:#224077" target="_blank">American International University-Bangladesh (AIUB)</a> and since then working as Web Developer
+              and had some experience on building multi-vendor E-commerce platforms.
             </p>
-            <h4>C</h4>
+            <h4>Currently working with</h4>
             <b-navbar type="light" variant="warning">
               <h3>
+                PHP, Codeigniter, Laravel, Javascript, Vue.js, React, Next/Nuxt, Node.js, Ruby and Python
               </h3>
             </b-navbar>
             <br />
             <div>
-              <h4></h4>
+              <h4>Check my works and profile</h4>
                 <ul class="ul">
                   <li>
+                    <a href="https://github.com/MahmudJion" style="text-decoration: none; color:#24292E" target="_blank">GitHub</a>
+                  </li>
+                  <li>
+                    <a href="https://www.linkedin.com/in/mahmud-hasan-jion/" style="text-decoration: none; color:#0A66C2" target="_blank">Linkedin</a>
+                  </li>
+                  <li>
+                    <a href="https://twitter.com/mahmudhasanjion" style="text-decoration: none; color:#1A91DA" target="_blank">Twitter</a>
+                  </li>
+                  <li>
+                    <a href="https://stackoverflow.com/users/10030431/mahmud-hasan-jion" style="text-decoration: none; color:#F3862F" target="_blank">Stack Overflow</a>
+                  </li>
+                  <li>
+                    <a href="https://scholar.google.com/citations?user=rvFKahIAAAAJ&hl=en" style="text-decoration: none; color:#767676" target="_blank">Google Scholar</a>
+                  </li>
+                  <li>
+                    <a href="https://herts.academia.edu/MahmudHasanJion" style="text-decoration: none; color:#3C3F42" target="_blank">Academia.edu</a>
                   </li>
                 </ul>
             </div>
@@ -30,7 +50,7 @@
   </template>
 
   <script>
-  export default {};
+  export default {}
   </script>
 
   <style>
@@ -42,3 +62,7 @@
     justify-content: right;
     align-items: center;
   }
+  .ul {
+    list-style-type: square;
+  }
+  </style>
